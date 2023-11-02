ed202c0046f8caa6b4022aa0e537bc47d37112f4
Puth Vang <35241506+PuthVang@users.noreply.github.com>
2023-11-01 19:26:54 -0700

    Added in a basic main menu

--- app/src/main/java/com/example/snakio/SnakeActivity.java
+++ app/src/main/java/com/example/snakio/SnakeActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.View;
 
 public class SnakeActivity extends Activity {
 
@@ -21,6 +22,16 @@ public class SnakeActivity extends Activity {
         // Initialize the result into a Point object
         Point size = new Point();
         display.getSize(size);
+
+        //// This puts the game in full screen and
+        //// hides all the other things such as navigation bar.
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
         // Create a new instance of the SnakeEngine class
         mSnakeGame = new SnakeGame(this, size);
