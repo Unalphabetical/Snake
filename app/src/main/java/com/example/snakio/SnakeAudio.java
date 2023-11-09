0768e18e454be0ac18c5a6bb599cb98d185715ee
Kiezroy <kyletnguyen16@gmail.com>
2023-11-09 10:02:18 -0800

    Added in background music

--- app/src/main/java/com/example/snakio/SnakeAudio.java
+++ app/src/main/java/com/example/snakio/SnakeAudio.java
@@ -5,17 +5,25 @@ import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.os.Build;
 
 import java.io.IOException;
 
 public class SnakeAudio {
+
+    private Context context;
     private SoundPool mSP;
     private int mEat_ID = -1;
     private int mCrashID = -1;
 
+    //Added MediaPlayer for background music
+    private MediaPlayer mediaPlayer;
+
     public SnakeAudio(Context context) {
+        this.context = context;
+
         // Initialize the SoundPool
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             AudioAttributes audioAttributes = new AudioAttributes.Builder()
@@ -30,19 +38,35 @@ public class SnakeAudio {
         } else {
             mSP = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
         }
+        // Initialize MediaPlayer for background music
+        mediaPlayer = new MediaPlayer();
         try {
-            AssetManager assetManager = context.getAssets();
-            AssetFileDescriptor descriptor;
+            AssetFileDescriptor afd = context.getAssets().openFd("background_music.ogg");
+            mediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+            afd.close();
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mediaPlayer.prepare();
+            mediaPlayer.setVolume(1f, 1f);
+            mediaPlayer.setLooping(true);
+            if (!mediaPlayer.isPlaying()) {
+                playBackgroundMusic();
+            }
 
-            // Prepare the sounds in memory
-            descriptor = assetManager.openFd("get_apple.ogg");
-            mEat_ID = mSP.load(descriptor, 0);
+            // Load other sounds
+            try {
+                AssetManager assetManager = context.getAssets();
+                AssetFileDescriptor descriptor;
 
-            descriptor = assetManager.openFd("snake_death.ogg");
-            mCrashID = mSP.load(descriptor, 0);
+                descriptor = assetManager.openFd("get_apple.ogg");
+                mEat_ID = mSP.load(descriptor, 0);
 
+                descriptor = assetManager.openFd("snake_death.ogg");
+                mCrashID = mSP.load(descriptor, 0);
+            } catch (IOException e) {
+                //Handle errors loading other sounds
+            }
         } catch (IOException e) {
-            // Error handling
+            //Handle errors loading background music
         }
     }
 
@@ -52,6 +76,20 @@ public class SnakeAudio {
 
     public void playCrashSound() {
         mSP.play(mCrashID, 1, 1, 0, 0, 1);
+    }
+
+    // Background music methods
+    public void playBackgroundMusic() {
+        mediaPlayer.start();
+
+    }
+
+    public void pauseBackgroundMusic() {
+        mediaPlayer.pause();
+    }
+
+    public boolean isBackgroundMusicPaused() {
+        return !mediaPlayer.isPlaying();
     }
 }
 
